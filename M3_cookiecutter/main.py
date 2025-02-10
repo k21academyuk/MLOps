891c4e1f68b26fe00f841c46f98c7e0217476b33
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2025-02-10 16:13:23 +0530

    Uploading python main.py

---
+++ M3_cookiecutter/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    """Main function to demonstrate NumPy usage."""
+    arr = np.array([1, 2, 3])
+    print("Numpy Array:", arr)
+
+if __name__ == "__main__":
+    main()
