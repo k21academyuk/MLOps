404ffb1480bceddd9d109a83c5a9beeb0c7c2b58
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2025-02-13 12:45:36 +0530

    Delete data-science-template directory

--- data-science-template/hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def parse_dependencies_settings():
-    if "{{ cookiecutter.dependency_manager }}" != "pip":
-        os.remove("requirements.txt")
-        os.remove("requirements-dev.txt")
-
-if __name__ == "__main__":
-    parse_dependencies_settings()
